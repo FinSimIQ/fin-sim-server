8c5430f58e5057785a63dc02adf43177c952b310
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-09-18 13:26:30 -0400

    connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-// const env = require("dotenv");
-// env.config();
+const env = require("dotenv");
+env.config();
+
+require("./models/db");
 
 // import libraries
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+app.use(cors({ origin: "*" }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process?.env?.PORT || 8081;
 app.listen(port, () => {
