e159efbb277395f1e00e2907ad1ee469fe99e0b2
SurajS1234 <suraj.samudrala3151@gmail.com>
2024-10-13 22:46:02 -0400

    Backend commits for listing quizzes
    Quiz.schema - Updated the schema file to add a subject parameter to filter quizzes by subject
    Quiz.controller - Controller file for listing quizzes, has a method to list all quizzes and list quizzes by subject
    Quiz.routes - Router file for the listing quizzes methods

---
+++ Quiz.schema.js
@@ -0,0 +1,31 @@
+// quiz model
+
+// import mongoose
+const mongoose = require("mongoose");
+
+// define quiz model schema
+const quizSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  subject: { 
+    type: String, 
+    required: true,
+    enum: [
+      'Stock Market', 
+      'Personal Finance', 
+      'Fintech', 
+      'Investment', 
+      'Risk Management', 
+      'Financial Analysis', 
+      'Quantitative Finance', 
+      'Financial Modeling', 
+      'Trading'
+    ] // Restrict subjects to the predefined nine categories
+  },
+  questions: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Question", required: true },
+  ],
+});
+
+// export quiz model
+mongoose.model("Quiz", quizSchema);
