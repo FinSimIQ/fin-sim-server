ae90b5e02b5052d67683da631ef653d81921fa11
Dev-Patel18 <143572779+Dev-Patel18@users.noreply.github.com>
2024-10-17 14:07:36 -0400

    Create Quiz.model.js

---
+++ src/models/Quiz.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// define quiz model schema
+const quizSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  questions: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Question", required: true },
+  ],
+});
+
+// export quiz model
+module.exports = mongoose.model("Quiz", quizSchema);
